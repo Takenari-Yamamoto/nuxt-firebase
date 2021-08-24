85d0ef9fdda4c1beaaf3ee2a110030e87bab862e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2021-08-25 01:21:27 +0900

    ログイン画面作成

---
+++ components/TheLoginComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="the-login-component">
+    <h1 class="title">ログインするかぁ？</h1>
+    <input class="form" type="text" placeholder="Email">
+    <input class="form" type="text" placeholder="Password">
+    <button class="btn">Login</button>
+  </div>
+</template>
+
+<style>
+.the-login-component {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  border: solid 1px;
+  border-radius: 14px;
+  padding: 2rem;
+}
+
+.title {
+  text-align: center;
+}
+
+.form {
+  margin-bottom :1.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+.btn {
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <Tutorial/>
+  <div class="index">
+    <h1 class="title">Nuxt と Firebase で遊ぼう❤️</h1>
+    <TheLoginComponent class="login" />
+  </div>
 </template>
 
 <script>
-export default {}
+import TheLoginComponent from "../components/TheLoginComponent";
+
+export default {
+  components: {
+    TheLoginComponent
+  }
+}
 </script>
+
+<style scoped>
+.index {
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.title {
+  margin-bottom: 3rem;
+}
+
+</style>
