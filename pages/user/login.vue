f04e79074b4674147a5638754429f24d02b78a1a
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2021-09-09 09:10:45 +0900

    create register login page (not used yet)

---
+++ pages/user/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="title">ログインしますか？</h1>
+    <input v-model="email" class="form" type="text" placeholder="Email" />
+    <input v-model="password" class="form" type="text" placeholder="Password" />
+    <button
+      class="btn -red"
+      @click="
+        $store.dispatch('user/login', {
+          email: email,
+          password: password,
+        })
+      "
+    >
+      ログイン
+    </button>
+    <button class="btn -white">会員登録をしてない方はこちら</button>
+  </div>
+</template>
---
+++ pages/user/register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="title">会員登録しますか？</h1>
+    <input v-model="email" class="form" type="text" placeholder="Email" />
+    <input v-model="password" class="form" type="text" placeholder="Password" />
+    <button
+      class="btn -red"
+      @click="
+        $store.dispatch('user/register', {
+          email: email,
+          password: password,
+        })
+      "
+    >
+      会員登録
+    </button>
+    <button class="btn -white" @click="alreadyRegister = !alreadyRegister">
+      すでに会員の方はこちら
+    </button>
+  </div>
+</template>
