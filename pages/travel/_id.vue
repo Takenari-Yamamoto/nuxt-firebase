5106985dd59adbf3ff2d864f3848db3f34b4918f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2021-09-13 00:50:44 +0900

    詳細ページリファクタ

--- pages/travel/_id.vue
+++ pages/travel/_id.vue
@@ -3,12 +3,13 @@
     <NuxtChild />
     <div v-if="!$route.path.match(/edit/)">
       <div class="travel-detail">
-        <h2>投稿詳細です</h2>
-        <nuxt-link :to="post.postId + '/edit'">編集</nuxt-link>
-        <p>Post id: {{ post.postId }}</p>
-        <p class="uid">User id: {{ post.uid }} さんの投稿です</p>
-        <p class="title">Title: {{ post.title }}</p>
-        <p class="content">Content: {{ post.content }}</p>
+        <p class="id">Post id {{ post.postId }}</p>
+        <p class="uid">User id: {{ post.uid }}</p>
+        <nuxt-link :to="post.postId + '/edit'" class="btn -white"
+          >編集</nuxt-link
+        >
+        <p class="title">{{ post.title }}</p>
+        <p class="content">{{ post.content }}</p>
       </div>
     </div>
   </div>
